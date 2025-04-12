02a666a1e9002df5f11eae4f31930c2b7a223a2c
amreshgupta <amreshgupta365@gmail.com>
2025-04-12 23:39:25 +0530

    array

---
+++ array/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+ func main()  {
+	fmt.Println("Here array in go lang")
+	var name[3]string
+	name[0]="mak"
+	name[1]="hey"
+	var numbers=[5]int {1,2,3,4,5}
+
+	fmt.Println("name of person:",numbers)
+	fmt.Println(len(numbers))
+ }
